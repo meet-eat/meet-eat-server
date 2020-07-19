3e087f8c20b8543fae6bad0980cbb2fb32af126b
Moritz Gstuer <moritz@gstuer.com>
2020-07-19 20:26:35 +0200

    Add missing imports to EntityController

--- src/main/java/meet_eat/server/controller/EntityController.java
+++ src/main/java/meet_eat/server/controller/EntityController.java
@@ -1,18 +1,18 @@
 package meet_eat.server.controller;
 
 import meet_eat.data.entity.Entity;
+import meet_eat.server.service.EntityService;
+import meet_eat.server.service.security.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public abstract class EntityController<T extends Entity, U, K extends EntityService<T, U>> {
 
-    @Autowired
     private final K entityService;
-
-    @Autowired
     private final SecurityService<T> securityService;
 
+    @Autowired
     protected EntityController(K entityService, SecurityService<T> securityService) {
         this.entityService = entityService;
         this.securityService = securityService;
