2bca8f9533f040bd227f30314b343dcee226df2f
Moritz Gstuer <moritz@gstuer.com>
2020-07-19 19:20:51 +0200

    Add TokenService class frame

---
+++ src/main/java/meet_eat/server/service/TokenService.java
@@ -0,0 +1,25 @@
+package meet_eat.server.service;
+
+import meet_eat.data.LoginCredential;
+import meet_eat.data.entity.Token;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public class TokenService extends EntityService<Token, String> {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public TokenService(MongoRepository<Token, String> entityRepository, UserRepository userRepository) {
+        super(entityRepository);
+        this.userRepository = userRepository;
+    }
+
+    public Token createToken(LoginCredential loginCredential) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public boolean isValidToken(Token token) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+}
