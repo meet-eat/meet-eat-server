3710c0d6e61a9f59cbcb858cf53d310fed4eb6d9
Moritz Gstuer <moritz@gstuer.com>
2020-07-19 19:02:35 +0200

    Add implementation of EntityService class

---
+++ src/main/java/meet_eat/server/service/EntityService.java
@@ -0,0 +1,41 @@
+package meet_eat.server.service;
+
+import meet_eat.data.entity.Entity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public class EntityService<T extends Entity, U> {
+
+    private final MongoRepository<T, U> entityRepository;
+
+    @Autowired
+    public EntityService(MongoRepository<T, U> entityRepository) {
+        this.entityRepository = entityRepository;
+    }
+
+    public Iterable<T> getAll() {
+        return entityRepository.findAll();
+    }
+
+    public Optional<T> get(U identifier) {
+        return entityRepository.findById(identifier);
+    }
+
+    public T post(T entity) {
+        return entityRepository.insert(entity);
+    }
+
+    public T put(T entity) {
+        return entityRepository.save(entity);
+    }
+
+    public void delete(T entity) {
+        entityRepository.delete(entity);
+    }
+
+    public void delete(U identifier) {
+        entityRepository.deleteById(identifier);
+    }
+}
