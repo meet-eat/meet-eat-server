6e8f5c1b37996827f31a893985db0fa71305a849
Moritz Gstuer <moritz@gstuer.com>
2020-07-20 22:28:05 +0200

    Add quasi-enumeration for endpoint path strings

---
+++ src/main/java/meet_eat/server/controller/EndpointPath.java
@@ -0,0 +1,10 @@
+package meet_eat.server.controller;
+
+public final class EndpointPath {
+
+    public static final String LOGIN = "/login";
+    public static final String LOGOUT = "/logout";
+
+    private EndpointPath() {
+    }
+}
