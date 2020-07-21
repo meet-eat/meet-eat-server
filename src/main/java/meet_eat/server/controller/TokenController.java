da0d7081f4ae09562115d25d647b8d57859198e0
Moritz Gstuer <moritz@gstuer.com>
2020-07-21 21:28:18 +0200

    Add token-controller and -service functionality

--- src/main/java/meet_eat/server/controller/TokenController.java
+++ src/main/java/meet_eat/server/controller/TokenController.java
@@ -1,10 +1,17 @@
 package meet_eat.server.controller;
 
+import meet_eat.data.LoginCredential;
 import meet_eat.data.entity.Token;
 import meet_eat.server.service.TokenService;
 import meet_eat.server.service.security.TokenSecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 public class TokenController extends EntityController<Token, String, TokenService> {
@@ -12,5 +19,25 @@ public class TokenController extends EntityController<Token, String, TokenServic
     @Autowired
     public TokenController(TokenService tokenService, TokenSecurityService tokenSecurityService) {
         super(tokenService, tokenSecurityService);
+    }
+
+    @PostMapping(EndpointPath.LOGIN)
+    public ResponseEntity<Token> login(@RequestBody LoginCredential loginCredential) {
+        if (Objects.isNull(loginCredential)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else if (!getEntityService().isValidLoginCredential(loginCredential)) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+        Token token = getEntityService().createToken(loginCredential);
+        return new ResponseEntity<>(token, HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping(EndpointPath.LOGOUT)
+    public ResponseEntity<Void> logout(@RequestBody Token token) {
+        if (Objects.isNull(token)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        getEntityService().delete(token);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
--- src/main/java/meet_eat/server/service/TokenService.java
+++ src/main/java/meet_eat/server/service/TokenService.java
@@ -1,13 +1,24 @@
 package meet_eat.server.service;
 
+import com.google.common.base.Charsets;
+import com.google.common.hash.Hashing;
 import meet_eat.data.LoginCredential;
 import meet_eat.data.entity.Token;
+import meet_eat.data.entity.user.User;
 import meet_eat.server.repository.TokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Optional;
+
 @Service
 public class TokenService extends EntityService<Token, String, TokenRepository> {
+
+    private static final String ERROR_MESSAGE_INVALID_LOGIN_CREDENTIALS = "Given login credentials must be valid.";
+    private static final int SALT_BYTE_LENGTH = 16;
 
     private final UserService userService;
 
@@ -18,10 +29,43 @@ public class TokenService extends EntityService<Token, String, TokenRepository> 
     }
 
     public Token createToken(LoginCredential loginCredential) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        // Check whether the user exists and login credentials are valid.
+        User user = userService.getUserByEmail(loginCredential.getEmail());
+        if (Objects.isNull(user) || !isValidLoginCredential(loginCredential)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_INVALID_LOGIN_CREDENTIALS);
+        }
+
+        // Generate a random salt for the token hash value
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[SALT_BYTE_LENGTH];
+        random.nextBytes(salt);
+
+        // Concat the salt's bytes, email and current time to get a string with "high entropy", randomness respectively.
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte elem : salt) {
+            stringBuilder.append(elem);
+        }
+        stringBuilder.append(loginCredential.getEmail());
+        stringBuilder.append(LocalDateTime.now());
+
+        // Hash the generated string, create the token and insert it into the repository.
+        String tokenValue = Hashing.sha256().hashString(stringBuilder, Charsets.UTF_16).toString();
+        return getRepository().insert(new Token(user, tokenValue));
+    }
+
+    public boolean isValidLoginCredential(LoginCredential loginCredential) {
+        if (Objects.isNull(loginCredential)) {
+            return false;
+        }
+        User user = userService.getUserByEmail(loginCredential.getEmail());
+        return Objects.nonNull(user) && user.getPassword().equals(loginCredential.getPassword());
     }
 
     public boolean isValidToken(Token token) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (Objects.isNull(token)) {
+            return false;
+        }
+        Optional<Token> repoToken = getRepository().findById(token.getIdentifier());
+        return repoToken.isPresent() && token.equals(repoToken.get());
     }
 }
