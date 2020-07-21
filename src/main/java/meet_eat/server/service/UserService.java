759caf473b44ab8748d1576ed969b1d3a146b0f9
Moritz Gstuer <moritz@gstuer.com>
2020-07-21 21:17:46 +0200

    Add find by email to user service and repo

--- src/main/java/meet_eat/server/service/UserService.java
+++ src/main/java/meet_eat/server/service/UserService.java
@@ -1,5 +1,6 @@
 package meet_eat.server.service;
 
+import meet_eat.data.entity.user.Email;
 import meet_eat.data.entity.user.User;
 import meet_eat.server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,5 +12,9 @@ public class UserService extends EntityService<User, String, UserRepository> {
     @Autowired
     public UserService(UserRepository userRepository) {
         super(userRepository);
+    }
+
+    public User getUserByEmail(Email email) {
+        return getRepository().findOneByEmail();
     }
 }
