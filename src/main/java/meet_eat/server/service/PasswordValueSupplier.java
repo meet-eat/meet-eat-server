43c3d66fc7940285993f4ca45f979a7ac787d5c7
Moritz Gstuer <moritz@gstuer.com>
2020-07-23 23:55:00 +0200

    Add password value supplier class frame

---
+++ src/main/java/meet_eat/server/service/PasswordValueSupplier.java
@@ -0,0 +1,11 @@
+package meet_eat.server.service;
+
+import java.util.function.Supplier;
+
+public class PasswordValueSupplier implements Supplier<String> {
+
+    @Override
+    public String get() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+}
