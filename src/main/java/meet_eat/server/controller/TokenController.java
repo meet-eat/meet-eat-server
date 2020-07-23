85074afd91fe0ea44ac8793b3d27625939147628
Moritz Gstuer <moritz@gstuer.com>
2020-07-23 15:21:20 +0200

    Delete unneeded import in token controller

--- src/main/java/meet_eat/server/controller/TokenController.java
+++ src/main/java/meet_eat/server/controller/TokenController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
 @RestController
 public class TokenController extends EntityController<Token, String, TokenService> {
 
