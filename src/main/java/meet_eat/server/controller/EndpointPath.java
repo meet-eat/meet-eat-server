db1f0e8c77ad5daeb4c52c53f5d47b120dab7eaf
Moritz Gstuer <moritz@gstuer.com>
2020-07-23 19:19:26 +0200

    Implement TagController functionality

--- src/main/java/meet_eat/server/controller/EndpointPath.java
+++ src/main/java/meet_eat/server/controller/EndpointPath.java
@@ -4,6 +4,7 @@ public final class EndpointPath {
 
     public static final String LOGIN = "/login";
     public static final String LOGOUT = "/logout";
+    public static final String TAGS = "/tags";
 
     private EndpointPath() {
     }
--- src/main/java/meet_eat/server/controller/TagController.java
+++ src/main/java/meet_eat/server/controller/TagController.java
@@ -1,9 +1,19 @@
 package meet_eat.server.controller;
 
+import meet_eat.data.RequestHeaderField;
 import meet_eat.data.entity.Tag;
+import meet_eat.data.entity.Token;
 import meet_eat.server.service.TagService;
 import meet_eat.server.service.security.TagSecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +22,49 @@ public class TagController extends EntityController<Tag, String, TagService> {
     @Autowired
     public TagController(TagService tagService, TagSecurityService tagSecurityService) {
         super(tagService, tagSecurityService);
+    }
+
+    // GET
+
+    @GetMapping(EndpointPath.TAGS)
+    public ResponseEntity<Iterable<Tag>> getAllTags(@RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
+        return handleGetAll(token);
+    }
+
+    @GetMapping(EndpointPath.TAGS + URI_PATH_SEGMENT_IDENTIFIER)
+    public ResponseEntity<Tag> getTag(@PathVariable(value = PATH_VARIABLE_IDENTIFIER) String identifier,
+                                      @RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
+        return handleGet(identifier, token);
+    }
+
+    // POST
+
+    @PostMapping(EndpointPath.TAGS)
+    public ResponseEntity<Tag> postTag(@RequestBody Tag tag,
+                                       @RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
+        return handlePost(tag, token);
+    }
+
+    // PUT
+
+    @PutMapping(EndpointPath.TAGS + URI_PATH_SEGMENT_IDENTIFIER)
+    public ResponseEntity<Tag> putTag(@PathVariable(value = PATH_VARIABLE_IDENTIFIER, required = false) String identifier,
+                                      @RequestBody Tag tag,
+                                      @RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
+        return handlePut(identifier, tag, token);
+    }
+
+    // DELETE
+
+    @DeleteMapping(EndpointPath.TAGS)
+    public ResponseEntity<Void> deleteTag(@RequestBody Tag tag,
+                                          @RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
+        return handleDelete(tag, token);
+    }
+
+    @DeleteMapping(EndpointPath.TAGS + URI_PATH_SEGMENT_IDENTIFIER)
+    public ResponseEntity<Void> deleteTag(@PathVariable(value = PATH_VARIABLE_IDENTIFIER) String identifier,
+                                          @RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
+        return handleDelete(identifier, token);
     }
 }
