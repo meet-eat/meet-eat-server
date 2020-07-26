c02b39217153eb1d097c58189879fc148795c834
Moritz Gstuer <moritz@gstuer.com>
2020-07-26 13:44:28 +0200

    Add user specific security

--- src/main/java/meet_eat/server/service/security/UserSecurityService.java
+++ src/main/java/meet_eat/server/service/security/UserSecurityService.java
@@ -1,6 +1,7 @@
 package meet_eat.server.service.security;
 
 import meet_eat.data.entity.Token;
+import meet_eat.data.entity.user.Role;
 import meet_eat.data.entity.user.User;
 import meet_eat.server.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,21 +18,27 @@ public class UserSecurityService extends SecurityService<User> {
 
     @Override
     public boolean isLegalGet(Token authenticationToken) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return isValidAuthentication(authenticationToken);
     }
 
     @Override
     public boolean isLegalPost(User entity, Token authenticationToken) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        boolean isAuthenticatedAdmin = isValidAuthentication(authenticationToken) &&
+                authenticationToken.getUser().getRole().equals(Role.ADMIN);
+
+        return isAuthenticatedAdmin || entity.getRole().equals(Role.USER);
     }
 
     @Override
     public boolean isLegalPut(User entity, Token authenticationToken) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        boolean isAdmin = authenticationToken.getUser().getRole().equals(Role.ADMIN);
+        boolean isEntityOwner = authenticationToken.getUser().equals(entity);
+
+        return isValidAuthentication(authenticationToken) && (isAdmin || isEntityOwner);
     }
 
     @Override
     public boolean isLegalDelete(User entity, Token authenticationToken) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return isLegalPut(entity, authenticationToken);
     }
 }
