9fdb57c0a0b5781d1c87a5b28045031ea546b825
Moritz Gstuer <moritz@gstuer.com>
2020-07-26 13:57:48 +0200

    Add placeholder for token specific security

--- src/main/java/meet_eat/server/service/security/TokenSecurityService.java
+++ src/main/java/meet_eat/server/service/security/TokenSecurityService.java
@@ -3,7 +3,6 @@ package meet_eat.server.service.security;
 import meet_eat.data.entity.Token;
 import meet_eat.server.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,21 +15,21 @@ public class TokenSecurityService extends SecurityService<Token> {
 
     @Override
     public boolean isLegalGet(Token authenticationToken) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean isLegalPost(Token entity, Token authenticationToken) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean isLegalPut(Token entity, Token authenticationToken) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean isLegalDelete(Token entity, Token authenticationToken) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        throw new UnsupportedOperationException();
     }
 }
