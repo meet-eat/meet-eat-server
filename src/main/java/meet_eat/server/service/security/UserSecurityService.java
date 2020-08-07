547a4c3a191901ea6f24d813789ee0cca5525d17
Moritz Gstuer <moritz@gstuer.com>
2020-08-07 02:22:05 +0200

    Add javadoc to user security service

--- src/main/java/meet_eat/server/service/security/UserSecurityService.java
+++ src/main/java/meet_eat/server/service/security/UserSecurityService.java
@@ -5,12 +5,19 @@ import meet_eat.data.entity.user.Role;
 import meet_eat.data.entity.user.User;
 import meet_eat.server.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 
+/**
+ * Represents a service class providing security relevant functionality mainly for {@link User users}.
+ */
 @Service
 public class UserSecurityService extends SecurityService<User> {
 
+    /**
+     * Constructs a new {@link UserSecurityService} instance.
+     *
+     * @param tokenService the {@link TokenService} to be used by the {@link UserSecurityService}
+     */
     @Autowired
     protected UserSecurityService(TokenService tokenService) {
         super(tokenService);
