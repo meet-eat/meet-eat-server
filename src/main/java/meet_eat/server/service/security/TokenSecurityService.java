0ce8c44057d46fbf4eebb7ac5d2e7296dca1f1a5
Moritz Gstuer <moritz@gstuer.com>
2020-08-07 02:20:30 +0200

    Add javadoc to token security service

--- src/main/java/meet_eat/server/service/security/TokenSecurityService.java
+++ src/main/java/meet_eat/server/service/security/TokenSecurityService.java
@@ -5,9 +5,17 @@ import meet_eat.server.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Represents a service class providing security relevant functionality mainly for {@link Token tokens}.
+ */
 @Service
 public class TokenSecurityService extends SecurityService<Token> {
 
+    /**
+     * Constructs a new {@link TokenSecurityService} instance.
+     *
+     * @param tokenService the {@link TokenService} to be used by the {@link TokenSecurityService}
+     */
     @Autowired
     protected TokenSecurityService(TokenService tokenService) {
         super(tokenService);
