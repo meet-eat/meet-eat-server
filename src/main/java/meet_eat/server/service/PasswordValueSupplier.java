c914762a59ea358f8ca292c4288a9ccd4f6a2768
Moritz Gstuer <moritz@gstuer.com>
2020-08-07 02:47:29 +0200

    Add javadoc to password value supplier

--- src/main/java/meet_eat/server/service/PasswordValueSupplier.java
+++ src/main/java/meet_eat/server/service/PasswordValueSupplier.java
@@ -7,16 +7,37 @@ import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+/**
+ * Represents a {@link Supplier} class for {@link meet_eat.data.entity.user.Password} compatible {@link String} values.
+ */
 public class PasswordValueSupplier implements Supplier<String> {
 
+    /**
+     * Represents the possible special characters used for password value creation.
+     */
     private static final char[] SPECIAL_CHARACTERS = "!#$%&*_+,-./:;'<=>?@^|~(){}".toCharArray();
+
+    /**
+     * Represents the possible basic characters, letters respectively, used for password value creation.
+     */
     private static final char[] BASIC_CHARACTERS = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ".toCharArray();
+
+    /**
+     * Represents the possible digits used for password value creation.
+     */
     private static final char[] DIGITS = "1234567890".toCharArray();
 
     private int basicCharCount;
     private int specialCharCount;
     private int digitCount;
 
+    /**
+     * Constructs a new instance of {@link PasswordValueSupplier} with the given character and digit amounts.
+     *
+     * @param basicCharCount   the number of basic characters used for password value creation
+     * @param specialCharCount the number of special characters used for password value creation
+     * @param digitCount       the number of digits used for password value creation
+     */
     public PasswordValueSupplier(int basicCharCount, int specialCharCount, int digitCount) {
         this.basicCharCount = basicCharCount;
         this.specialCharCount = specialCharCount;
@@ -51,26 +72,56 @@ public class PasswordValueSupplier implements Supplier<String> {
         return characters.stream().map(String::valueOf).collect(Collectors.joining());
     }
 
+    /**
+     * Gets the numbers of basic characters used for password value creation.
+     *
+     * @return The number of basic characters.
+     */
     public int getBasicCharCount() {
         return basicCharCount;
     }
 
+    /**
+     * Gets the numbers of special characters used for password value creation.
+     *
+     * @return The number of special characters.
+     */
     public int getSpecialCharCount() {
         return specialCharCount;
     }
 
+    /**
+     * Gets the numbers of digits used for password value creation.
+     *
+     * @return The number of digits.
+     */
     public int getDigitCount() {
         return digitCount;
     }
 
+    /**
+     * Sets the numbers of basic characters used for password value creation.
+     *
+     * @param basicCharCount the number of basic characters
+     */
     public void setBasicCharCount(int basicCharCount) {
         this.basicCharCount = basicCharCount;
     }
 
+    /**
+     * Sets the numbers of special characters used for password value creation.
+     *
+     * @param specialCharCount the number of special characters
+     */
     public void setSpecialCharCount(int specialCharCount) {
         this.specialCharCount = specialCharCount;
     }
 
+    /**
+     * Sets the numbers of digits used for password value creation.
+     *
+     * @param digitCount the number of digits
+     */
     public void setDigitCount(int digitCount) {
         this.digitCount = digitCount;
     }
