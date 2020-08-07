254cb30b9ae5ac98fd34a28b7e2f6dbfbf3282e5
Moritz Gstuer <moritz@gstuer.com>
2020-08-07 02:36:17 +0200

    Add javadoc to entity conflict exception

--- src/main/java/meet_eat/server/service/EntityConflictException.java
+++ src/main/java/meet_eat/server/service/EntityConflictException.java
@@ -1,11 +1,22 @@
 package meet_eat.server.service;
 
+/**
+ * Represents an exception indicating a conflict between {@link meet_eat.data.entity.Entity} instances.
+ */
 public class EntityConflictException extends IllegalArgumentException {
 
+    /**
+     * Constructs a new instance of {@link EntityConflictException}.
+     */
     public EntityConflictException() {
         super();
     }
 
+    /**
+     * Constructs a new instance of {@link EntityConflictException} with a given message.
+     *
+     * @param message the message of the exception
+     */
     public EntityConflictException(String message) {
         super(message);
     }
