6234d7b4fcf3c98cfa588355bcc00a1874c88aec
Moritz Gstuer <moritz@gstuer.com>
2020-08-08 23:44:31 +0200

    Add javadoc to tag service class

--- src/main/java/meet_eat/server/service/TagService.java
+++ src/main/java/meet_eat/server/service/TagService.java
@@ -6,9 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+/**
+ * Represents a service class providing functionality to manage {@link Tag tags} and their state persistence.
+ */
 @Service
 public class TagService extends EntityService<Tag, String, TagRepository> {
 
+    /**
+     * Constructs a new instance of {@link TagService}.
+     *
+     * @param tagRepository the repository used for persistence operations
+     */
     @Lazy
     @Autowired
     public TagService(TagRepository tagRepository) {
