89b9bf390bf369cfd41383787b84a12daf8a9bab
Moritz Gstuer <moritz@gstuer.com>
2020-08-09 00:39:23 +0200

    Add javadoc to header property editor class

--- src/main/java/meet_eat/server/HeaderPropertyEditor.java
+++ src/main/java/meet_eat/server/HeaderPropertyEditor.java
@@ -4,10 +4,19 @@ import meet_eat.data.ObjectJsonParser;
 
 import java.beans.PropertyEditorSupport;
 
+/**
+ * Represents a custom data serialization and deserialization class especially for header elements used by
+ * {@link meet_eat.server.controller.EntityController controllers} to receive and send messages correctly.
+ */
 public class HeaderPropertyEditor extends PropertyEditorSupport {
 
     private final Class<?> headerClass;
 
+    /**
+     * Constructs a new instance of {@link HeaderPropertyEditor}.
+     *
+     * @param headerClass {@link Class class} of the received or sent header element
+     */
     public HeaderPropertyEditor(Class<?> headerClass) {
         this.headerClass = headerClass;
     }
