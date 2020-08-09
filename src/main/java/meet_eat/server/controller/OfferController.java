1f9970753c57be68446b620d12689ef8805d591c
Moritz Gstuer <moritz@gstuer.com>
2020-08-09 02:22:19 +0200

    Add javadoc to offer controller class

--- src/main/java/meet_eat/server/controller/OfferController.java
+++ src/main/java/meet_eat/server/controller/OfferController.java
@@ -9,8 +9,10 @@ import meet_eat.data.entity.Offer;
 import meet_eat.data.entity.Token;
 import meet_eat.data.entity.user.User;
 import meet_eat.data.predicate.OfferPredicate;
+import meet_eat.server.service.EntityService;
 import meet_eat.server.service.OfferService;
 import meet_eat.server.service.security.OfferSecurityService;
+import meet_eat.server.service.security.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,13 +34,31 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Represents an concrete controller class handling incoming RESTful CRUD requests by providing specific endpoints
+ * especially for {@link Offer} entities.
+ */
 @RestController
 public class OfferController extends EntityController<Offer, String, OfferService> {
 
+    /**
+     * Represents a request parameter descriptor for an {@link Offer offer's} owner.
+     */
     protected static final String REQUEST_PARAM_OWNER = "owner";
+
+    /**
+     * Represents a request parameter descriptor for a {@link User subscriber}.
+     */
     protected static final String REQUEST_PARAM_SUBSCRIBER = "subscriber";
+
     private static final String URI_PATH_SEGMENT_PARTICIPANTS = "/participants";
 
+    /**
+     * Constructs a new instace of {@link OfferController}.
+     *
+     * @param offerService         the {@link EntityService} used by this controller
+     * @param offerSecurityService the {@link SecurityService} used by this controller
+     */
     @Autowired
     public OfferController(OfferService offerService, OfferSecurityService offerSecurityService) {
         super(offerService, offerSecurityService);
@@ -46,12 +66,29 @@ public class OfferController extends EntityController<Offer, String, OfferServic
 
     // GET
 
+    /**
+     * Gets an specific persistent {@link Offer} from the persistence layer.
+     *
+     * @param identifier the identifier of the offer to be got
+     * @param token      the authentication token of the requester
+     * @return an specific identified offer entity within a {@link ResponseEntity}
+     */
     @GetMapping(EndpointPath.OFFERS + URI_PATH_SEGMENT_IDENTIFIER)
     public ResponseEntity<Offer> getOffer(@PathVariable(value = PATH_VARIABLE_IDENTIFIER) String identifier,
                                           @RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
         return handleGet(identifier, token);
     }
 
+    /**
+     * Gets all persistent {@link Offer offers} from the persistence layer.
+     *
+     * @param creatorIdentifier    the identifier of an offer's {@link User creator}
+     * @param subscriberIdentifier the identifier of an {@link User user} searching for offers of subscriptions
+     * @param predicates           the {@link OfferPredicate predicates} used for filtering the returned offers
+     * @param comparator           the {@link OfferComparator comparator} used for sorting the returned offers
+     * @param token                the authentication token of the requester
+     * @return all available offers filtered and sorted within a {@link ResponseEntity}
+     */
     @GetMapping(EndpointPath.OFFERS)
     public ResponseEntity<Iterable<Offer>> getAllOffers(
             @RequestParam(value = REQUEST_PARAM_OWNER, required = false) String creatorIdentifier,
@@ -116,12 +153,27 @@ public class OfferController extends EntityController<Offer, String, OfferServic
 
     // POST
 
+    /**
+     * Posts a new {@link Offer offer} into the persistence layer.
+     *
+     * @param offer the offer to be posted
+     * @param token the authentication token of the requester
+     * @return the posted offer within a {@link ResponseEntity}
+     */
     @PostMapping(EndpointPath.OFFERS)
     public ResponseEntity<Offer> postOffer(@RequestBody Offer offer,
                                            @RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
         return handlePost(offer, token);
     }
 
+    /**
+     * Posts a new participant of an {@link Offer offer} into the persistence layer.
+     *
+     * @param identifier  the identifier of the offer
+     * @param participant the participant to be posted
+     * @param token       the authentication token of the requester
+     * @return the offer containing the posted participant within a {@link ResponseEntity}
+     */
     @PostMapping(EndpointPath.OFFERS + URI_PATH_SEGMENT_IDENTIFIER + URI_PATH_SEGMENT_PARTICIPANTS)
     public ResponseEntity<Offer> postParticipant(@PathVariable(value = PATH_VARIABLE_IDENTIFIER) String identifier,
                                                  @RequestBody User participant,
@@ -155,12 +207,27 @@ public class OfferController extends EntityController<Offer, String, OfferServic
 
     // PUT
 
+    /**
+     * Puts a modified {@link Offer offer} into the persistence layer.
+     *
+     * @param offer the offer to be put
+     * @param token the authentication token of the requester
+     * @return the put offer within a {@link ResponseEntity}
+     */
     @PutMapping(EndpointPath.OFFERS)
     public ResponseEntity<Offer> putOffer(@RequestBody Offer offer,
                                           @RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
         return handlePut(null, offer, token);
     }
 
+    /**
+     * Puts a modified {@link Offer offer} into the persistence layer.
+     *
+     * @param identifier the identifier of the offer to be put
+     * @param offer      the offer to be put
+     * @param token      the authentication token of the requester
+     * @return the put offer within a {@link ResponseEntity}
+     */
     @PutMapping(EndpointPath.OFFERS + URI_PATH_SEGMENT_IDENTIFIER)
     public ResponseEntity<Offer> putOffer(@PathVariable(value = PATH_VARIABLE_IDENTIFIER) String identifier,
                                           @RequestBody Offer offer,
@@ -170,18 +237,40 @@ public class OfferController extends EntityController<Offer, String, OfferServic
 
     // DELETE
 
+    /**
+     * Deletes an {@link Offer offer} from the persistence layer.
+     *
+     * @param offer the offer to be deleted
+     * @param token the authentication token of the requester
+     * @return a bodiless {@link ResponseEntity}
+     */
     @DeleteMapping(EndpointPath.OFFERS)
     public ResponseEntity<Void> deleteOffer(@RequestBody Offer offer,
                                             @RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
         return handleDelete(offer, token);
     }
 
+    /**
+     * Deletes an {@link Offer offer} from the persistence layer.
+     *
+     * @param identifier the identifier of an offer to be deleted
+     * @param token      the authentication token of the requester
+     * @return a bodiless {@link ResponseEntity}
+     */
     @DeleteMapping(EndpointPath.OFFERS + URI_PATH_SEGMENT_IDENTIFIER)
     public ResponseEntity<Void> deleteOffer(@PathVariable(value = PATH_VARIABLE_IDENTIFIER) String identifier,
                                             @RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
         return handleDelete(identifier, token);
     }
 
+    /**
+     * Deletes a {@link User participant} form an {@link Offer offer} within the persistence layer.
+     *
+     * @param identifier  the identifier of the offer
+     * @param participant the participant to be deleted
+     * @param token       the authentication token of the requester
+     * @return the offer without the deleted participant within a {@link ResponseEntity}
+     */
     @DeleteMapping(EndpointPath.OFFERS + URI_PATH_SEGMENT_IDENTIFIER + URI_PATH_SEGMENT_PARTICIPANTS)
     public ResponseEntity<Offer> deleteParticipant(@PathVariable(value = PATH_VARIABLE_IDENTIFIER) String identifier,
                                                    @RequestBody User participant,
@@ -211,6 +300,12 @@ public class OfferController extends EntityController<Offer, String, OfferServic
         return new ResponseEntity<>(getEntityService().put(offer), HttpStatus.OK);
     }
 
+    /**
+     * Filters a given {@link Iterable} of {@link Offer offers}.
+     * @param offers the offers to be filtered
+     * @param predicates the {@link OfferPredicate predicates} used for filtering
+     * @return the filtered offers
+     */
     private static Iterable<Offer> filterOffers(Iterable<Offer> offers, OfferPredicate... predicates) {
         Stream<Offer> offerStream = Streams.stream(offers);
         for (OfferPredicate predicate : predicates) {
