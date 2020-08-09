0025ce5e995c21636eb96548b1a7ed485f665a87
Moritz Gstuer <moritz@gstuer.com>
2020-08-09 02:36:17 +0200

    Add javadoc to subscription controller class

--- src/main/java/meet_eat/server/controller/SubscriptionController.java
+++ src/main/java/meet_eat/server/controller/SubscriptionController.java
@@ -6,6 +6,7 @@ import meet_eat.data.RequestHeaderField;
 import meet_eat.data.entity.Subscription;
 import meet_eat.data.entity.Token;
 import meet_eat.data.entity.user.User;
+import meet_eat.server.service.EntityService;
 import meet_eat.server.service.SubscriptionService;
 import meet_eat.server.service.security.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +24,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Represents an concrete controller class handling incoming RESTful CRUD requests by providing specific endpoints
+ * especially for {@link Subscription} entities.
+ */
 @RestController
 public class SubscriptionController extends EntityController<Subscription, String, SubscriptionService> {
 
+    /**
+     * Constructs a new instance of {@link SubscriptionController}.
+     *
+     * @param entityService   the {@link EntityService} used by this controller
+     * @param securityService the {@link SecurityService} used by this controller
+     */
     @Lazy
     @Autowired
     public SubscriptionController(SubscriptionService entityService, SecurityService<Subscription> securityService) {
@@ -34,6 +45,13 @@ public class SubscriptionController extends EntityController<Subscription, Strin
 
     // GET
 
+    /**
+     * Gets all persistent {@link Subscription subscriptions} of an identified user from the persistence layer.
+     *
+     * @param userIdentifier the identifier of the {@link User} whose subscriptions are requested
+     * @param token          the authentication token of the requester
+     * @return all requested subscriptions within a {@link ResponseEntity}
+     */
     @GetMapping(EndpointPath.USERS + URI_PATH_SEGMENT_IDENTIFIER + EndpointPath.SUBSCRIPTIONS)
     public ResponseEntity<Iterable<Subscription>> getSubscriptionsByUser(@PathVariable(value = PATH_VARIABLE_IDENTIFIER) String userIdentifier,
                                                                          @RequestHeader(value = RequestHeaderField.TOKEN, required = false) Token token) {
@@ -55,6 +73,14 @@ public class SubscriptionController extends EntityController<Subscription, Strin
 
     // Post
 
+    /**
+     * Posts a new {@link Subscription subscription} into the persistence layer.
+     *
+     * @param userIdentifier the identifier of the {@link User source user} of the subscription
+     * @param subscription   the subscription to be posted
+     * @param token          the authentication token of the requester
+     * @return the posted subscription within a {@link ResponseEntity}
+     */
     @PostMapping(EndpointPath.USERS + URI_PATH_SEGMENT_IDENTIFIER + EndpointPath.SUBSCRIPTIONS)
     public ResponseEntity<Subscription> postSubscription(@PathVariable(value = PATH_VARIABLE_IDENTIFIER) String userIdentifier,
                                                          @RequestBody Subscription subscription,
@@ -67,6 +93,14 @@ public class SubscriptionController extends EntityController<Subscription, Strin
 
     // Delete
 
+    /**
+     * Deletes a {@link Subscription subscription} from the persistence layer.
+     *
+     * @param userIdentifier the {@link User source user} of the subscription
+     * @param subscribedUser the {@link User target user} of the subscription
+     * @param token          the authentication token of the requester
+     * @return a bodiless {@link ResponseEntity}
+     */
     @DeleteMapping(EndpointPath.USERS + URI_PATH_SEGMENT_IDENTIFIER + EndpointPath.SUBSCRIPTIONS)
     public ResponseEntity<Void> deleteSubscriptionBySubscribedUser(@PathVariable(value = PATH_VARIABLE_IDENTIFIER) String userIdentifier,
                                                                    @RequestBody User subscribedUser,
