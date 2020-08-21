39f4eb189e0f55f53e396e3a8fb5e313f8d9cd78
Moritz Gstuer <moritz@gstuer.com>
2020-08-21 21:04:43 +0200

    Generalized abstract entity relation service

--- src/main/java/meet_eat/server/service/EntityRelationService.java
+++ src/main/java/meet_eat/server/service/EntityRelationService.java
@@ -6,43 +6,27 @@ import meet_eat.server.repository.EntityRelationRepository;
 
 import java.io.Serializable;
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * Represents a service class providing functionality to manage {@link EntityRelation entity relations} and their state
  * persistence.
  *
- * @param <K>  the type of the {@link EntityRelation entity relation}
- * @param <T>  the type of the {@link Entity source entity} within the relation
- * @param <S>  the type of the {@link Entity target entity} within the relation
- * @param <UK> the type of the identifier of the {@link EntityRelation entity relation}
- * @param <UT> the type of the identifier of the {@link Entity source entity}
- * @param <US> the type of the identifier of the {@link Entity target entity}
- * @param <R>  the type of the {@link EntityRelation entity relation} {@link EntityRelationRepository repository}
- * @param <ST> the type of the {@link Entity source entity} {@link EntityService service}
- * @param <SS> the type of the {@link Entity target entity} {@link EntityService service}
+ * @param <K> the type of the {@link EntityRelation entity relation}
+ * @param <T> the type of the {@link Entity source entity} within the relation
+ * @param <S> the type of the {@link Entity target entity} within the relation
+ * @param <R> the type of the {@link EntityRelation entity relation} {@link EntityRelationRepository repository}
  */
-public abstract class EntityRelationService<K extends EntityRelation<T, S, UK>,
-        T extends Entity<UT>, S extends Entity<US>,
-        UK extends Serializable, UT extends Serializable, US extends Serializable,
-        R extends EntityRelationRepository<K, T, S, UK>,
-        ST extends EntityService<T, UT, ?>, SS extends EntityService<S, US, ?>>
-        extends EntityService<K, UK, R> {
-
-    private final ST sourceService;
-    private final SS targetService;
+public abstract class EntityRelationService<K extends EntityRelation<T, S, U>, T extends Entity<?>,
+        S extends Entity<?>, U extends Serializable, R extends EntityRelationRepository<K, T, S, U>>
+        extends EntityService<K, U, R> {
 
     /**
      * Constructs a new instance of {@link EntityRelationService}.
      *
-     * @param repository    the repository used for persistence operations
-     * @param sourceService the service used for operations on and with {@link Entity source entities}
-     * @param targetService the service used for operations on and with {@link Entity target entities}
+     * @param repository the repository used for persistence operations
      */
-    protected EntityRelationService(R repository, ST sourceService, SS targetService) {
+    protected EntityRelationService(R repository) {
         super(repository);
-        this.sourceService = sourceService;
-        this.targetService = targetService;
     }
 
     /**
@@ -84,26 +68,6 @@ public abstract class EntityRelationService<K extends EntityRelation<T, S, UK>,
     }
 
     /**
-     * Deletes all {@link EntityRelation relations} containing a specific {@link Entity source entity}.
-     *
-     * @param sourceIdentifier the identifier of the source entity of the relations to be deleted
-     */
-    public void deleteBySourceIdentifier(UT sourceIdentifier) {
-        Optional<T> optionalSource = sourceService.get(Objects.requireNonNull(sourceIdentifier));
-        optionalSource.ifPresent(this::deleteBySource);
-    }
-
-    /**
-     * Deletes all {@link EntityRelation relations} containing a specific {@link Entity target entity}.
-     *
-     * @param targetIdentifier the identifier of the target entity of the relations to be deleted
-     */
-    public void deleteByTargetIdentifier(US targetIdentifier) {
-        Optional<S> optionalTarget = targetService.get(Objects.requireNonNull(targetIdentifier));
-        optionalTarget.ifPresent(this::deleteByTarget);
-    }
-
-    /**
      * Deletes a {@link EntityRelation relation} containing a specific {@link Entity source entity} and
      * {@link Entity target entity}.
      *
@@ -135,23 +99,5 @@ public abstract class EntityRelationService<K extends EntityRelation<T, S, UK>,
      */
     public boolean existsBySourceAndTarget(T source, S target) {
         return getRepository().existsBySourceAndTarget(Objects.requireNonNull(source), Objects.requireNonNull(target));
-    }
-
-    /**
-     * Gets the {@link EntityService} used for working on and with {@link Entity source entities}.
-     *
-     * @return the source entity service
-     */
-    public ST getSourceService() {
-        return sourceService;
-    }
-
-    /**
-     * Gets the {@link EntityService} used for working on and with {@link Entity target entities}.
-     *
-     * @return the target entity service
-     */
-    public SS getTargetService() {
-        return targetService;
     }
 }
