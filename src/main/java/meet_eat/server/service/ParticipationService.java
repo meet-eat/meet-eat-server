65c7a11fa4b4d550e69b7706eb811c7cc265d3c2
Moritz Gstuer <moritz@gstuer.com>
2020-08-22 14:46:41 +0200

    Add javadoc to undocumented method

--- src/main/java/meet_eat/server/service/ParticipationService.java
+++ src/main/java/meet_eat/server/service/ParticipationService.java
@@ -3,6 +3,7 @@ package meet_eat.server.service;
 import meet_eat.data.entity.Offer;
 import meet_eat.data.entity.relation.Participation;
 import meet_eat.data.entity.user.User;
+import meet_eat.server.repository.OfferRepository;
 import meet_eat.server.repository.ParticipationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -31,6 +32,14 @@ public class ParticipationService extends EntityRelationService<Participation, U
         this.offerService = offerService;
     }
 
+    /**
+     * Gets all {@link Participation participations} containing a specific {@link Offer offer}.
+     * If the given offer identifier cannot be found within the {@link OfferRepository}
+     * an empty {@link Optional} is returned.
+     *
+     * @param offerIdentifier the offer's identifier
+     * @return all relations containing a specific offer within an {@link Optional}
+     */
     public Optional<Iterable<Participation>> getByOfferIdentifier(String offerIdentifier) {
         Optional<Offer> optionalOffer = offerService.get(offerIdentifier);
         return optionalOffer.map(this::getByTarget);
