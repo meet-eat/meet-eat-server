934aea889ba64c15ee1ec5922651fa49af25793f
Moritz Gstuer <moritz@gstuer.com>
2020-08-27 13:34:59 +0200

    Fix import style of test class

--- src/test/java/meet_eat/server/service/EntityConflictExceptionTest.java
+++ src/test/java/meet_eat/server/service/EntityConflictExceptionTest.java
@@ -1,8 +1,10 @@
 package meet_eat.server.service;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class EntityConflictExceptionTest {
 
