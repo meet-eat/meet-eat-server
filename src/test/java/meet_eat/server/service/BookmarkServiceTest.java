9fff69a9ee103314e0fc69597204c356efe601c5
Moritz Gstuer <moritz@gstuer.com>
2020-08-27 18:14:20 +0200

    Add missing tests to bookmark service test class

--- src/test/java/meet_eat/server/service/BookmarkServiceTest.java
+++ src/test/java/meet_eat/server/service/BookmarkServiceTest.java
@@ -1,10 +1,141 @@
 package meet_eat.server.service;
 
+import com.google.common.collect.Iterables;
 import meet_eat.data.entity.Offer;
 import meet_eat.data.entity.relation.Bookmark;
 import meet_eat.data.entity.user.User;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class BookmarkServiceTest extends EntityRelationServiceTest<BookmarkService, Bookmark, User, Offer, String> {
+
+    private static final class BookmarkMock extends Bookmark {
+
+        private static final long serialVersionUID = 8238669973606994892L;
+
+        protected BookmarkMock(String identifier, User source, Offer target) {
+            super(identifier, source, target);
+        }
+    }
+
+    //#region @Test getByUserIdentifier
+
+    @Test(expected = NullPointerException.class)
+    public void testGetByUserIdentifierNull() {
+        // Assertions
+        getEntityService().getByUserIdentifier(null);
+    }
+
+    @Test
+    public void testGetByUserIdentifierNonExistingUserEmpty() {
+        // Test data
+        String identifier = "ABCDEFG123!";
+
+        // Execution
+        Optional<Iterable<Bookmark>> optionalBookmarks = getEntityService().getByUserIdentifier(identifier);
+
+        // Assertions
+        assertTrue(optionalBookmarks.isEmpty());
+    }
+
+    @Test
+    public void testGetByUserIdentifierExistingUserEmpty() {
+        // Test data
+        User user = getBasicUser();
+
+        // Execution
+        Optional<Iterable<Bookmark>> optionalBookmarks = getEntityService().getByUserIdentifier(user.getIdentifier());
+
+        // Assertions
+        assertTrue(optionalBookmarks.isPresent());
+        assertNotNull(optionalBookmarks.get());
+        assertEquals(0, Iterables.size(optionalBookmarks.get()));
+    }
+
+    @Test
+    public void testGetByUserIdentifierSingleBookmark() {
+        // Test data
+        User user = getBasicUser();
+        Bookmark bookmark = getRelationEntity(user, getTargetEntity());
+
+        // Execution
+        Optional<Iterable<Bookmark>> optionalBookmarks = getEntityService().getByUserIdentifier(user.getIdentifier());
+
+        // Assertions
+        assertTrue(optionalBookmarks.isPresent());
+        assertNotNull(optionalBookmarks.get());
+        assertEquals(1, Iterables.size(optionalBookmarks.get()));
+        assertTrue(Iterables.contains(optionalBookmarks.get(), bookmark));
+    }
+
+    @Test
+    public void testGetByUserIdentifierMultipleBookmarks() {
+        // Test data
+        User user = getBasicUser();
+        Bookmark bookmarkFst = getRelationEntity(user, getTargetEntity());
+        Bookmark bookmarkSnd = getRelationEntity(user, getTargetEntity());
+        Bookmark bookmarkForeign = getRelationEntity(getSourceEntity(), getTargetEntity());
+
+        // Execution
+        Optional<Iterable<Bookmark>> optionalBookmarks = getEntityService().getByUserIdentifier(user.getIdentifier());
+
+        // Assertions
+        assertEquals(2, Iterables.size(optionalBookmarks.orElseThrow()));
+        assertTrue(Iterables.contains(optionalBookmarks.get(), bookmarkFst));
+        assertTrue(Iterables.contains(optionalBookmarks.get(), bookmarkSnd));
+    }
+
+    //#endregion
+
+    //#region @Test existsPostConflict
+
+    @Test
+    public void testExistsPostConflictSameIdentifier() {
+        // Test data
+        Bookmark bookmark = getRelationEntity(getSourceEntity(), getTargetEntity());
+        Bookmark conflictBookmark = new BookmarkMock(bookmark.getIdentifier(), getSourceEntity(), getTargetEntity());
+
+        // Assertions
+        assertTrue(getEntityService().existsPostConflict(conflictBookmark));
+    }
+
+    @Test
+    public void testExistsPostConflictSameSourceAndSameTarget() {
+        // Test data
+        Bookmark bookmark = getRelationEntity(getSourceEntity(), getTargetEntity());
+        Bookmark conflictBookmark = new Bookmark(bookmark.getSource(), bookmark.getTarget());
+
+        // Assertions
+        assertTrue(getEntityService().existsPostConflict(conflictBookmark));
+    }
+
+    @Test
+    public void testExistsPostConflictDifferentSourceAndSameTarget() {
+        // Test data
+        Bookmark bookmark = getRelationEntity(getSourceEntity(), getTargetEntity());
+        Bookmark conflictBookmark = new Bookmark(getSourceEntity(), bookmark.getTarget());
+
+        // Assertions
+        assertFalse(getEntityService().existsPostConflict(conflictBookmark));
+    }
+
+    @Test
+    public void testExistsPostConflictSameSourceAndDifferentTarget() {
+        // Test data
+        Bookmark bookmark = getRelationEntity(getSourceEntity(), getTargetEntity());
+        Bookmark conflictBookmark = new Bookmark(bookmark.getSource(), getTargetEntity());
+
+        // Assertions
+        assertFalse(getEntityService().existsPostConflict(conflictBookmark));
+    }
+
+    //#endregion
 
     @Override
     protected User getSourceEntity() {
